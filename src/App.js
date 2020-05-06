e76e407d3f2fe31c236b55dcbec55f0ba1b7fa9f
Robert Botarleanu <robertbotarleanu@gmail.com>
2020-05-06 19:04:14 +0300

    Add code for initial version

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Toolbar from './Toolbar.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Toolbar className="toolbar"/>
+        <p>
+          Hello
+        </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Slider.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import RangeSlider from 'react-bootstrap-range-slider';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import './Slider.css';
+
+export default class Slider extends Component {
+
+    constructor(props) {
+        super(props);
+        this.label = props.label;
+
+        if (props.isPercentage) {
+            this.labelHandler = this.tooltipLabelPerc;
+        } else {
+            this.labelHandler = this.tooltipLabelNumber;
+        }
+
+        this.state = {
+            sliderProps: props.sliderProps
+        };
+        this.changeSliderValue.bind(this);
+    }
+
+    tooltipLabelPerc(crtValue) {
+        return crtValue + "%";
+    }
+
+    tooltipLabelNumber(crtValue) {
+        return crtValue;
+    }
+
+    changeSliderValue(changeEvent) {
+        // Need to do this conversion since otherwise an error is sometimes thrown
+        let newSliderValue = Number(changeEvent.target.value);
+        console.log("New slider value:", newSliderValue);
+        this.setState({
+            sliderProps: {
+                currentValue: newSliderValue
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="SliderParentDiv">
+                <div className="LabelChildDiv">
+                    {this.label}
+                </div>
+                <div className="SliderChildDiv">
+                    <RangeSlider
+                        value={this.state.sliderProps.currentValue}
+                        min={this.state.sliderProps.min}
+                        max={this.state.sliderProps.max}
+                        onChange={this.changeSliderValue}
+                        tooltip="on"
+                        tooltipLabel={this.labelHandler}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
