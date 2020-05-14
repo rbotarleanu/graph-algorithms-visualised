f4ba2d626fa5ae04eec625d348ccba9221dd5a37
Robert Botarleanu <robertbotarleanu@gmail.com>
2020-05-14 18:35:43 +0300

    Use react-draggable to optimize the dragging performance

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/Node.css';
+import Draggable from 'react-draggable';
+
+
 
 
 export default class Node extends Component {
@@ -9,68 +12,46 @@ export default class Node extends Component {
 
         this.changePositionNotification = props.changePositionNotification;
         this.nodeId = props.nodeId;
+        this.nodeRef = React.createRef();
 
         this.state = {
             posX: props.posX,
             posY: props.posY,
             radius: props.radius,
-            dragging: false,
-            cursorOffsetX: 0,
-            cursorOffsetY: 0
+            draggableX: props.posX,
+            draggableY: props.posY
         }
-        // this.ctx = canvas.getContext("2d");
-        this.dragStart = this.dragStart.bind(this);
-        this.dragEnd = this.dragEnd.bind(this);
-    }
-
-    dragStart(e) {
-        this.setState({
-            dragging: true,
-            cursorOffsetX: e.pageX - this.state.posX,
-            cursorOffsetY: e.pageY - this.state.posY
-        });
-        
-        e.stopPropagation();
-        e.preventDefault();
-
-    }
-
-    dragEnd(e) {
-        this.setState({dragging: false});
-
-        e.stopPropagation();
-        e.preventDefault();
     }
 
     dragMove(e) {
-        if (!this.state.dragging) {
-            return;
-        }
+        console.log(e);
 
         this.setState({
-            posX: e.pageX - this.state.cursorOffsetX,
-            posY: e.pageY - this.state.cursorOffsetY,
+            draggableX: this.state.draggableX + e.movementX,
+            draggableY: this.state.draggableY + e.movementY,
         });
 
         e.stopPropagation();
         e.preventDefault();
 
         this.changePositionNotification(
-            this.nodeId, this.state.posX, this.state.posY);
+            this.nodeId, this.state.draggableX, this.state.draggableY);
     }
 
     render() {
         return (
+            <Draggable 
+                nodeRef={this.nodeRef}
+                onDrag={e => this.dragMove(e)}
+                >
             <circle
                 cx={this.state.posX}
                 cy={this.state.posY}
+                ref={this.nodeRef}
                 r={this.state.radius}
                 fill="red"
-                onMouseDown={e => this.dragStart(e)}
-                onMouseMove={e => this.dragMove(e)}
-                onMouseUp={e => this.dragEnd(e)}
-                onMouseLeave={e => this.dragEnd(e)}
                 />
+            </Draggable>
         )
     }
 }
