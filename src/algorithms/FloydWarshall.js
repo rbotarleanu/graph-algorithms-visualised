cb5203009d86f5681767738a5337b881d9f48925
Robert Botarleanu <robertbotarleanu@gmail.com>
2020-07-15 19:29:10 +0300

    Modify Floyd-Warshall to show each step for each loop iteration

--- src/algorithms/FloydWarshall.js
+++ src/algorithms/FloydWarshall.js
@@ -21,45 +21,28 @@ export class FloydWarshall {
 
         this.step = this.step.bind(this);
 
-        this.crtStep = 0;
-        this.maxSteps = Number.MAX_VALUE;
-
         this.distances = {};
         this.backLinks = {};
         this.k = 0;
         this.visited = {};
+        this.numNodes = 0;
+
+        this.k = 0;
+        this.i = 0;
+        this.j = 0;
     }
 
     step(nodes, edges) {
-        if (this.crtStep === 0) {
-            for (var nodeId in nodes) {
-                this.distances[nodeId] = {};
-                this.backLinks[nodeId] = {};
-                for (var otherNodeId in nodes) {
-                    this.distances[nodeId][otherNodeId] = otherNodeId === nodeId ? 0 : Infinity;
-                    this.backLinks[nodeId][otherNodeId] = nodeId;
-                }
-            }
-
-            for (var edgeId in edges) {
-                var node1 = edges[edgeId].getNode1();
-                var node2 = edges[edgeId].getNode2();
-                var weight = edges[edgeId].getWeight();
-
-                this.distances[node1][node2] = weight;
-                this.backLinks[node1][node2] = node1;
-                
-                if (!edges[edgeId].isDirected()) {
-                    this.distances[node2][node1] = weight;
-                    this.backLinks[node2][node1] = node2;
-                }
-            }
-
-            this.maxSteps = Object.keys(nodes).length;
-        } else if (this.crtStep === this.maxSteps) {
+        if (this.i + this.j + this.k === 0) {
+            this.initSearchAlgorithm(nodes, edges);
+        } else if (this.k === this.numNodes) {
             return null;
         }
-        console.log("crtstep", this.crtStep, this.maxSteps);
+
+        if (this.i === this.k || this.j === this.k || this.i === this.j) {
+            this.increment();
+            return this.step(nodes, edges);
+        }
 
         let findEdge = (u, v) => {
             for (var edgeId in edges) {
@@ -86,61 +69,109 @@ export class FloydWarshall {
         var nodeUpdates = [];
         var edgeUpdates = [];
         var relaxedEdges = [];
+        var updates = false;
 
-        this.relaxationNode = nodes[this.crtStep].getId();
+        let relaxationNode = nodes[this.k].getId();
+        let u = nodes[this.i].getId();
+        let v = nodes[this.j].getId();
 
-        var newDistances = {...this.distances};
+        let relaxedDist = this.distances[u][relaxationNode] + this.distances[relaxationNode][v];
 
-        for (var u in nodes) {
-            for (var v in nodes) {
-                let relaxedDist = this.distances[u][this.relaxationNode] + this.distances[this.relaxationNode][v];
+        if (this.distances[u][v] > relaxedDist) {
+            this.distances[u][v] = relaxedDist;
+            this.backLinks[u][v] = relaxationNode;
 
-                if (this.distances[u][v] > relaxedDist) {
-                    newDistances[u][v] = relaxedDist;
-                    this.backLinks[u][v] = this.relaxationNode;
+            let ukPath = findPath(u, relaxationNode);
+            let kvPath = findPath(relaxationNode, v);
+            let ukvPath = ukPath.concat(kvPath);
 
-                    let ukPath = findPath(u, this.relaxationNode);
-                    let kvPath = findPath(this.relaxationNode, v);
-                    let ukvPath = ukPath.concat(kvPath);
+            relaxedEdges = relaxedEdges.concat(ukvPath);
+            for (var edgeId in ukvPath) {
+                edgeUpdates.push(new EdgeUpdate(
+                    edgeId,
+                    undefined,
+                    true));
+                
+                updates = true;
+            }
+        }
 
-                    relaxedEdges = relaxedEdges.concat(ukvPath);
-                    for (var edgeId in ukvPath) {
-                        edgeUpdates.push(new EdgeUpdate(
-                            edgeId,
-                            undefined,
-                            'green'));
-                    }
+        for (var nodeId in nodes) {
+            var color = 'blue';
+            if (nodeId === relaxationNode) {
+                color = 'green';
+            } else if (nodeId === u || nodeId === v) {
+                color = 'yellow';
+            }
+
+            nodeUpdates.push(new NodeUpdate(
+                nodeId,
+                undefined, undefined,
+                color,
+                this.distances[nodeId]
+            ))
+        }
+
+        if (this.k > 0 && this.i == 0 && this.j === 1) {
+            for (var edgeId in edges) {
+                // reset highlights for edges not relaxed at this step
+                if (relaxedEdges.indexOf(edgeId) === -1) {
+                    edgeUpdates.push(new EdgeUpdate(
+                        edgeId,
+                        undefined,
+                        false
+                    ));
                 }
             }
         }
 
-        this.distances = newDistances;
-
-        for (var nodeId in nodes) {
-            nodeUpdates.push(new NodeUpdate(
-                nodeId,
-                undefined,
-                undefined,
-                'green',
-                this.distances[nodeId]));    
-        }
-
-        for (var edgeId in edges) {
-            // reset highlights for edges not relaxed at this step
-            if (relaxedEdges.indexOf(edgeId) === -1) {
-                edgeUpdates.push(new EdgeUpdate(
-                    edgeId,
-                    undefined,
-                    'blue'
-                ));
-            }
-        }
-
-        this.crtStep += 1;
+        this.increment();
 
         return {
             nodeUpdates: nodeUpdates,
             edgeUpdates: edgeUpdates
         };
+    }
+
+    increment() {
+        this.j += 1;
+
+        if (this.j === this.numNodes) {
+            this.j = 0;
+            this.i += 1;
+        }
+
+        if (this.i === this.numNodes) {
+            this.i = 0;
+            this.j = 0;
+            this.k += 1;
+        }
+    }
+
+    initSearchAlgorithm(nodes, edges) {
+        this.numNodes = Object.keys(nodes).length;
+
+        for (var nodeId in nodes) {
+            this.distances[nodeId] = {};
+            this.backLinks[nodeId] = {};
+            for (var otherNodeId in nodes) {
+                this.distances[nodeId][otherNodeId] = otherNodeId === nodeId ? 0 : Infinity;
+                this.backLinks[nodeId][otherNodeId] = nodeId;
+            }
+        }
+
+        for (var edgeId in edges) {
+            var node1 = edges[edgeId].getNode1();
+            var node2 = edges[edgeId].getNode2();
+            var weight = edges[edgeId].getWeight();
+
+            this.distances[node1][node2] = weight;
+            this.backLinks[node1][node2] = node1;
+
+            if (!edges[edgeId].isDirected()) {
+                this.distances[node2][node1] = weight;
+                this.backLinks[node2][node1] = node2;
+            }
+        }
     }
 }
